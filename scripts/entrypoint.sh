ef4f31ac195d5e4a0df877b056d0b13b4e1102f2
Vania Vergara <vdrvergara@gmail.com>
2021-03-18 13:40:46 +0000

    Update entrypoint.sh
    Change to start-notebook.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,32 +1,20 @@
-#!/bin/sh
-#
-# Copyright (c) 2020 Red Hat, Inc.
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-#
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
 
-# Ensure $HOME exists when starting
-if [ ! -d "${HOME}" ]; then
-  mkdir -p "${HOME}"
+set -e
+
+wrapper=""
+if [[ "${RESTARTABLE}" == "yes" ]]; then
+    wrapper="run-one-constantly"
 fi
 
-# Setup $PS1 for a consistent and reasonable prompt
-if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
-  echo "PS1='\s-\v \w \$ '" > "${HOME}"/.bashrc
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+    # launched by JupyterHub, use single-user entrypoint
+    exec /usr/local/bin/start-singleuser.sh "$@"
+elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+    . /usr/local/bin/start.sh $wrapper jupyter lab "$@"
+else
+    echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
+    . /usr/local/bin/start.sh $wrapper jupyter notebook "$@"
 fi
-
-# Add current (arbitrary) user to /etc/passwd and /etc/group
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "update passwd file"
-    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
-    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
-  fi
-fi
-
-cat /etc/passwd
-id
-/usr/bin/supervisord -c /etc/supervisord.conf
